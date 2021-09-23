a9ddbf9cf404958b5e3fe69dfd16c9ffd854760d
joforsell <forsell.johan@hotmail.com>
2021-09-23 15:02:35 +0200

    Initial Commit

---
+++ BarTab/BarTabApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarTabApp.swift
+//  BarTab
+//
+//  Created by Johan Forsell on 2021-09-23.
+//
+
+import SwiftUI
+
+@main
+struct BarTabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
