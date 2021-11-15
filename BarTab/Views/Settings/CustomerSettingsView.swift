a5e38c8da20b4260cab1b9547c7ef45a3d2efcb7
joforsell <forsell.johan@hotmail.com>
2021-11-15 22:38:07 +0100

    Email toast and confirmation
    Added a toast to notify user of sent email and added a confirmation alert before sending email.

--- BarTab/Views/Settings/CustomerSettingsView.swift
+++ BarTab/Views/Settings/CustomerSettingsView.swift
@@ -6,18 +6,22 @@
 //
 
 import SwiftUI
+import AlertToast
 
 struct CustomerSettingsView: View {
     @EnvironmentObject var customerVM: CustomerViewModel
     @EnvironmentObject var userInfo: UserInfo
     
     @AppStorage("latestEmail") var latestEmail: Date = Date(timeIntervalSinceReferenceDate: 60000)
-
+    
     @State var editMode: EditMode = .inactive
     
     @State private var showError = false
     @State private var errorString = ""
-        
+    
+    @Binding var isShowingEmailSuccessToast: Bool
+    @State private var isShowingEmailConfirmation = false
+    
     var body: some View {
         VStack {
             List {
@@ -28,33 +32,16 @@ struct CustomerSettingsView: View {
             .navigationTitle("Medlemmar")
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        if -latestEmail.timeIntervalSinceNow > 86400 {
-                            customerVM.sendEmails(from: userInfo.user.association) { result in
-                                switch result {
-                                case .failure(let error):
-                                    errorString = error.localizedDescription
-                                    showError = true
-                                case .success( _):
-                                    latestEmail = Date()
-                                }
-                            }
-                            latestEmail = Date()
-                        } else {
-                            errorString = "Du kan inte göra mailutskick oftare än var 24:e timma."
-                            showError = true
+                    mailButton
+                        .alert(isPresented: $isShowingEmailConfirmation) {
+                            Alert(title: Text("Vill du göra ett mailutskick?"), message: Text("Vill du skicka ett mail med nuvarande saldo till samtliga kunder?"), primaryButton: .default(Text("OK"), action: emailButtonAction), secondaryButton: .destructive(Text("Avbryt")) { isShowingEmailConfirmation = false } )
                         }
-                    } label: {
-                        Image(systemName: "envelope.fill")
-                            .font(.largeTitle)
-                            .foregroundColor(-latestEmail.timeIntervalSinceNow > 86400 ? .black : .gray)
-                    }
                 }
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
-                            .font(.largeTitle)
-                            .foregroundColor(.black)
-                            .padding()
+                        .font(.largeTitle)
+                        .foregroundColor(.accentColor)
+                        .padding()
                 }
             }
             .environment(\.editMode, $editMode)
@@ -62,18 +49,43 @@ struct CustomerSettingsView: View {
         .alert(isPresented: $showError) {
             Alert(title: Text("Kunde inte göra mailutskick"), message: Text(errorString), dismissButton: .default(Text("OK")))
         }
+        
     }
     
-    func delete(at offsets: IndexSet) {
-        for index in offsets {
-            customerVM.removeCustomer(customerVM.customers[index].id!)
-        }
-        customerVM.customers.remove(atOffsets: offsets)
-    }
-    
-    func hasOneDayElapsedSinceLatestEmail(_ date: Date) -> Bool {
+    func oneDayHasElapsedSince(_ date: Date) -> Bool {
         let timeSinceLatestEmail = -latestEmail.timeIntervalSinceNow
-        return timeSinceLatestEmail > 86400
+        return timeSinceLatestEmail > 10
+    }
+    
+    var mailButton: some View {
+        Button {
+            if oneDayHasElapsedSince(latestEmail) {
+                isShowingEmailConfirmation.toggle()
+            } else {
+                errorString = "Du kan inte göra mailutskick oftare än var 24:e timma."
+                showError.toggle()
+            }
+        } label: {
+            Image(systemName: "envelope.fill")
+                .font(.largeTitle)
+                .foregroundColor(oneDayHasElapsedSince(latestEmail) ? .accentColor : .accentColor.opacity(0.3))
+        }
+    }
+    
+    func emailButtonAction() {
+        customerVM.sendEmails(from: userInfo.user.association) { result in
+            switch result {
+            case .failure(let error):
+                errorString = error.localizedDescription
+                showError = true
+            case .success( _):
+                latestEmail = Date()
+            }
+        }
+        latestEmail = Date()
+        withAnimation {
+            isShowingEmailSuccessToast.toggle()
+        }
     }
 }
 
@@ -104,6 +116,6 @@ struct CustomerRow: View {
 
 struct CustomerSettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        CustomerSettingsView()
+        CustomerSettingsView(isShowingEmailSuccessToast: .constant(false))
     }
 }
