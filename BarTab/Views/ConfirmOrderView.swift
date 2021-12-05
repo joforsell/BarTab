9716ffe546f89f982b52742f0cf6010bd312dd4a
joforsell <forsell.johan@hotmail.com>
2021-12-05 23:36:03 +0100

    Layout and add customer
    Worked on layout for drink cards and confirm purchase view. Added functionality of adding customers.

--- BarTab/Views/ConfirmOrderView.swift
+++ BarTab/Views/ConfirmOrderView.swift
@@ -16,22 +16,24 @@ struct ConfirmOrderView: View {
     var body: some View {
         ZStack(alignment: .bottomTrailing) {
             VStack(alignment: .leading) {
-                Text(drinkVM.drink.name).font(.title).bold()
+                Text(drinkVM.drink.name).font(.system(size: 80, weight: .black))
                     .matchedGeometryEffect(id: "\(drinkVM.drink.id ?? drinkVM.drink.name) drinkName", in: orderNamespace)
-                Text("\(drinkVM.drink.price) kr")
+                Text("\(drinkVM.drink.price) kr").font(.system(size: 40))
                     .matchedGeometryEffect(id: "\(drinkVM.drink.id ?? drinkVM.drink.name) drinkPrice", in: orderNamespace)
                 Spacer()
             }
             .foregroundColor(.white)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+            .padding(40)
             Image("beer")
                 .resizable()
-                .frame(width: 200, height: 250)
-                .foregroundColor(.accentColor)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: UIScreen.main.bounds.width * 0.2)
+                .foregroundColor(.accentColor.opacity(0.3))
+                .padding(20)
                 .matchedGeometryEffect(id: "\(drinkVM.drink.id ?? drinkVM.drink.name) image", in: orderNamespace)
         }
         .background(Color("AppBlue").matchedGeometryEffect(id: "\(drinkVM.drink.id ?? drinkVM.drink.name) view", in: orderNamespace))
-        .frame(width: 600, height: 600)
-
+        .cornerRadius(20)
     }
 }
